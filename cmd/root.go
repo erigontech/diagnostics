058bf91481d1243ad55d0d49f397df444c146440
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2023-02-23 12:35:12 +0000

    Dealing with http2

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,17 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"regexp"
+	"sync"
 	"syscall"
 	"time"
 
@@ -14,6 +19,11 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	"github.com/ledgerwatch/diagnostics/assets"
 )
 
 var (
@@ -67,7 +77,7 @@ func initConfig() {
 	}
 }
 
-var validPath = regexp.MustCompile("^/([a-zA-Z0-9]+)/(giveRequests|takeResponses|show)$")
+var validPath = regexp.MustCompile("^/([a-zA-Z0-9]+)$")
 
 func (dh *DiagHandler) giveRequestsHandler(w http.ResponseWriter, r *http.Request) {
 loop:
@@ -92,7 +102,70 @@ func showHandler(w http.ResponseWriter, r *http.Request) {
 type DiagHandler struct {
 	// Requests can be of arbitrary types, they get converted to a string and sent to the support subcommand
 	requestCh chan interface{}
+	cancel    context.CancelFunc
 }
+
+// Conn is client/server symmetric connection.
+// It implements the io.Reader/io.Writer/io.Closer to read/write or close the connection to the other side.
+// It also has a Send/Recv function to use channels to communicate with the other side.
+type Conn struct {
+	r  io.Reader
+	wc io.WriteCloser
+
+	cancel context.CancelFunc
+
+	wLock sync.Mutex
+	rLock sync.Mutex
+}
+
+func newConn(ctx context.Context, r io.Reader, wc io.WriteCloser) (*Conn, context.Context) {
+	ctx, cancel := context.WithCancel(ctx)
+	return &Conn{
+		r:      r,
+		wc:     wc,
+		cancel: cancel,
+	}, ctx
+}
+
+// Write writes data to the connection
+func (c *Conn) Write(data []byte) (int, error) {
+	c.wLock.Lock()
+	defer c.wLock.Unlock()
+	return c.wc.Write(data)
+}
+
+// Read reads data from the connection
+func (c *Conn) Read(data []byte) (int, error) {
+	c.rLock.Lock()
+	defer c.rLock.Unlock()
+	return c.r.Read(data)
+}
+
+// Close closes the connection
+func (c *Conn) Close() error {
+	c.cancel()
+	return c.wc.Close()
+}
+
+type flushWrite struct {
+	w io.Writer
+	f http.Flusher
+}
+
+func (w *flushWrite) Write(data []byte) (int, error) {
+	n, err := w.w.Write(data)
+	w.f.Flush()
+	return n, err
+}
+
+func (w *flushWrite) Close() error {
+	// Currently server side close of connection is not supported in Go.
+	// The server closes the connection when the http.Handler function returns.
+	// We use connection context and cancel function as a work-around.
+	return nil
+}
+
+var ErrHTTP2NotSupported = "HTTP2 not supported"
 
 func (dh *DiagHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	m := validPath.FindStringSubmatch(r.URL.Path)
@@ -100,43 +173,73 @@ func (dh *DiagHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	// User token is the first submatch m[1]
-	// TODO: validate the token
-	switch m[2] {
-	case "giveRequests":
-		dh.giveRequestsHandler(w, r)
-	case "takeResponses":
-		takeResponsesHandler(w, r)
-	case "show":
-		showHandler(w, r)
-	default:
-		http.NotFound(w, r)
+	if !r.ProtoAtLeast(2, 0) {
+		http.Error(w, ErrHTTP2NotSupported, http.StatusHTTPVersionNotSupported)
+		return
 	}
-}
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, ErrHTTP2NotSupported, http.StatusHTTPVersionNotSupported)
+		return
+	}
+	c, ctx := newConn(r.Context(), r.Body, &flushWrite{w: w, f: flusher})
+	defer c.Close()
 
-func (dh *DiagHandler) RequestNodeInfo() {
-	dh.requestCh <- "nodeInfo"
+	// Update the request context with the connection context.
+	// If the connection is closed by the server, it will also notify everything that waits on the request context.
+	*r = *r.WithContext(ctx)
+
+	w.WriteHeader(http.StatusOK)
+	flusher.Flush()
+
+	var writeBuf bytes.Buffer
+	for {
+		fmt.Fprintf(&writeBuf, "/db/list\n")
+		if _, err := c.Write(writeBuf.Bytes()); err != nil {
+			log.Printf("Writing metrics request: %v", err)
+			return
+		}
+		// Read the response
+		var sizeBuf [4]byte
+		if _, err := io.ReadFull(r.Body, sizeBuf[:]); err != nil {
+			log.Printf("Reading size of metrics response: %v", err)
+			return
+		}
+		metricsBuf := make([]byte, binary.BigEndian.Uint32(sizeBuf[:]))
+		if _, err := io.ReadFull(r.Body, metricsBuf); err != nil {
+			log.Printf("Reading metrics response: %v", err)
+			return
+		}
+		fmt.Printf("RESPONSE: \n%s\n", metricsBuf)
+	}
 }
 
 func webServer() error {
 	sigs := make(chan os.Signal, 1)
 	interruptCh := make(chan bool, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
+	ctx, cancel := context.WithCancel(context.Background())
 	go func() {
 		<-sigs
-		interruptCh <- true
+		cancel()
 	}()
+	h2s := &http2.Server{
+		// ...
+	}
+	mux := http.NewServeMux()
+	mux.Handle("/ui/", http.FileServer(http.FS(assets.Content)))
 	dh := &DiagHandler{requestCh: make(chan interface{}, 1024)}
+	mux.Handle("/", dh)
 	s := &http.Server{
-		Addr:           ":8080",
-		Handler:        dh,
+		Addr:           fmt.Sprintf("%s:%d", listenAddr, listenPort),
+		Handler:        h2c.NewHandler(mux, h2s),
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
+		ConnContext: func(_ context.Context, _ net.Conn) context.Context {
+			return ctx
+		},
 	}
-	// Initial request for node info
-	dh.RequestNodeInfo()
 	interrupt := false
 	go func() {
 		err := s.ListenAndServe()
