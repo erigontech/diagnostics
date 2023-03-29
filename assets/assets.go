c0be22d39b4e06a77a4c890d0d6ff52452657269
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2023-03-29 20:40:54 +0100

    First ui pages

---
+++ assets/assets.go
@@ -0,0 +1,6 @@
+package assets
+
+import "embed"
+
+//go:embed ui
+var Content embed.FS
--- cmd/root.go
+++ cmd/root.go
@@ -3,15 +3,18 @@ package cmd
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/binary"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"regexp"
-	"sync"
+	"strconv"
 	"syscall"
 	"time"
 
@@ -20,17 +23,17 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
-
 	"github.com/ledgerwatch/diagnostics/assets"
 )
 
 var (
 	// Used for flags.
-	cfgFile    string
-	listenAddr string
-	listenPort int
+	cfgFile        string
+	listenAddr     string
+	listenPort     int
+	serverKeyFile  string
+	serverCertFile string
+	caCertFiles    []string
 
 	rootCmd = &cobra.Command{
 		Use:   "diagnostics",
@@ -53,6 +56,11 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
 	rootCmd.Flags().StringVar(&listenAddr, "addr", "localhost", "network interface to listen on")
 	rootCmd.Flags().IntVar(&listenPort, "port", 8080, "port to listen on")
+	rootCmd.Flags().StringVar(&serverKeyFile, "tls.key", "", "path to server TLS key")
+	rootCmd.MarkFlagRequired("tls.key")
+	rootCmd.Flags().StringVar(&serverCertFile, "tls.cert", "", "paths to server TLS certificates")
+	rootCmd.MarkFlagRequired("tls.cert")
+	rootCmd.Flags().StringSliceVar(&caCertFiles, "tls.cacerts", []string{}, "comma-separated list of paths to and CAs TLS certificates")
 }
 
 func initConfig() {
@@ -77,98 +85,16 @@ func initConfig() {
 	}
 }
 
-var validPath = regexp.MustCompile("^/([a-zA-Z0-9]+)$")
-
-func (dh *DiagHandler) giveRequestsHandler(w http.ResponseWriter, r *http.Request) {
-loop:
-	for {
-		select {
-		case request := <-dh.requestCh:
-			fmt.Fprintf(w, "%s\n", request)
-		default:
-			break loop
-		}
-	}
-}
-
-func takeResponsesHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Take responses %s", r.URL.Path)
-}
-
-func showHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Show %s", r.URL.Path)
-}
-
-type DiagHandler struct {
-	// Requests can be of arbitrary types, they get converted to a string and sent to the support subcommand
-	requestCh chan interface{}
-	cancel    context.CancelFunc
-}
-
-// Conn is client/server symmetric connection.
-// It implements the io.Reader/io.Writer/io.Closer to read/write or close the connection to the other side.
-// It also has a Send/Recv function to use channels to communicate with the other side.
-type Conn struct {
-	r  io.Reader
-	wc io.WriteCloser
-
+type BridgeHandler struct {
 	cancel context.CancelFunc
-
-	wLock sync.Mutex
-	rLock sync.Mutex
 }
 
-func newConn(ctx context.Context, r io.Reader, wc io.WriteCloser) (*Conn, context.Context) {
-	ctx, cancel := context.WithCancel(ctx)
-	return &Conn{
-		r:      r,
-		wc:     wc,
-		cancel: cancel,
-	}, ctx
-}
-
-// Write writes data to the connection
-func (c *Conn) Write(data []byte) (int, error) {
-	c.wLock.Lock()
-	defer c.wLock.Unlock()
-	return c.wc.Write(data)
-}
-
-// Read reads data from the connection
-func (c *Conn) Read(data []byte) (int, error) {
-	c.rLock.Lock()
-	defer c.rLock.Unlock()
-	return c.r.Read(data)
-}
-
-// Close closes the connection
-func (c *Conn) Close() error {
-	c.cancel()
-	return c.wc.Close()
-}
-
-type flushWrite struct {
-	w io.Writer
-	f http.Flusher
-}
-
-func (w *flushWrite) Write(data []byte) (int, error) {
-	n, err := w.w.Write(data)
-	w.f.Flush()
-	return n, err
-}
-
-func (w *flushWrite) Close() error {
-	// Currently server side close of connection is not supported in Go.
-	// The server closes the connection when the http.Handler function returns.
-	// We use connection context and cancel function as a work-around.
-	return nil
-}
+var supportUrlRegex = regexp.MustCompile("^/support/([0-9]+)$")
 
 var ErrHTTP2NotSupported = "HTTP2 not supported"
 
-func (dh *DiagHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
+func (bh *BridgeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	m := supportUrlRegex.FindStringSubmatch(r.URL.Path)
 	if m == nil {
 		http.NotFound(w, r)
 		return
@@ -182,8 +108,9 @@ func (dh *DiagHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, ErrHTTP2NotSupported, http.StatusHTTPVersionNotSupported)
 		return
 	}
-	c, ctx := newConn(r.Context(), r.Body, &flushWrite{w: w, f: flusher})
-	defer c.Close()
+	ctx, cancel := context.WithCancel(r.Context())
+	defer cancel()
+	defer r.Body.Close()
 
 	// Update the request context with the connection context.
 	// If the connection is closed by the server, it will also notify everything that waits on the request context.
@@ -192,13 +119,22 @@ func (dh *DiagHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	flusher.Flush()
 
+	pin, err := strconv.ParseUint(m[1], 10, 64)
+	if err != nil {
+		log.Printf("Parsing pin %s: %v", m[1], err)
+	}
+	fmt.Printf("PIN: %d", pin)
+
 	var writeBuf bytes.Buffer
 	for {
+		//fmt.Printf("Sending request\n")
+		writeBuf.Reset()
 		fmt.Fprintf(&writeBuf, "/db/list\n")
-		if _, err := c.Write(writeBuf.Bytes()); err != nil {
+		if _, err := w.Write(writeBuf.Bytes()); err != nil {
 			log.Printf("Writing metrics request: %v", err)
 			return
 		}
+		flusher.Flush()
 		// Read the response
 		var sizeBuf [4]byte
 		if _, err := io.ReadFull(r.Body, sizeBuf[:]); err != nil {
@@ -210,53 +146,91 @@ func (dh *DiagHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			log.Printf("Reading metrics response: %v", err)
 			return
 		}
-		fmt.Printf("RESPONSE: \n%s\n", metricsBuf)
+		//fmt.Printf("RESPONSE: \n%s\n", metricsBuf)
+	}
+}
+
+type Session struct {
+}
+
+type SessionHandler struct {
+	sessions map[uint64]Session
+}
+
+var sessionUrlRegex = regexp.MustCompile("^/session/(new_session|resume_session)$")
+
+func (sh *SessionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	m := sessionUrlRegex.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Error parsing form: %v", err)
+		return
+	}
+	switch m[1] {
+	case "new_session":
+		fmt.Fprintf(w, "NEW_SESSION\n")
+	case "resume_session":
+		pinStr := r.FormValue("sessionid")
+		sessionPin, err := strconv.ParseUint(pinStr, 10, 64)
+		if err != nil {
+			fmt.Fprintf(w, "Error parsing session PIN %s: %v", pinStr, err)
+			return
+		}
+		_, ok := sh.sessions[sessionPin]
+		if !ok {
+			fmt.Fprintf(w, "Session %d is not found", sessionPin)
+			return
+		}
+		fmt.Fprintf(w, "RESUME SESSION: %s\n", sessionPin)
 	}
 }
 
 func webServer() error {
-	sigs := make(chan os.Signal, 1)
-	interruptCh := make(chan bool, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	ctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		<-sigs
-		cancel()
-	}()
-	h2s := &http2.Server{
-		// ...
-	}
 	mux := http.NewServeMux()
 	mux.Handle("/ui/", http.FileServer(http.FS(assets.Content)))
-	dh := &DiagHandler{requestCh: make(chan interface{}, 1024)}
-	mux.Handle("/", dh)
+	sh := &SessionHandler{
+		sessions: map[uint64]Session{},
+	}
+	mux.Handle("/session/", sh)
+	bh := &BridgeHandler{}
+	mux.Handle("/support/", bh)
+	certPool := x509.NewCertPool()
+	for _, caCertFile := range caCertFiles {
+		caCert, err := ioutil.ReadFile(caCertFile)
+		if err != nil {
+			log.Printf("Reading server certificate: %v", err)
+			return err
+		}
+		certPool.AppendCertsFromPEM(caCert)
+	}
+	tlsConfig := &tls.Config{
+		RootCAs: certPool,
+	}
 	s := &http.Server{
 		Addr:           fmt.Sprintf("%s:%d", listenAddr, listenPort),
-		Handler:        h2c.NewHandler(mux, h2s),
+		Handler:        mux,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 		ConnContext: func(_ context.Context, _ net.Conn) context.Context {
 			return ctx
 		},
+		TLSConfig: tlsConfig,
 	}
-	interrupt := false
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
-		err := s.ListenAndServe()
-		if err != nil {
-			log.Printf("Running server problem: %v", err)
-		}
+		<-sigs
+		cancel()
+		s.Shutdown(ctx)
 	}()
-	pollInterval := 500 * time.Millisecond
-	pollEvery := time.NewTicker(pollInterval)
-	defer pollEvery.Stop()
-	for !interrupt {
-		select {
-		case interrupt = <-interruptCh:
-			log.Printf("interrupted, please wait for cleanup")
-		case <-pollEvery.C:
-		}
+	err := s.ListenAndServeTLS(serverCertFile, serverKeyFile)
+	if err != nil {
+		log.Printf("Running server problem: %v", err)
 	}
-	s.Shutdown(context.Background())
 	return nil
 }
