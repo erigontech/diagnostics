fadc37153a20811baa4ecce2af397fcf524acec2
pawel49 <pawel@glassnode.com>
2023-05-10 17:05:34 +0200

    improve readability of cmd_line

--- cmd/cmd_line.go
+++ cmd/cmd_line.go
@@ -13,20 +13,18 @@ type CmdLineArgs struct {
 	Args    string
 }
 
-func processCmdLineArgs(w http.ResponseWriter, templ *template.Template, success bool, result string) {
+func processCmdLineArgs(w http.ResponseWriter, tmpl *template.Template, success bool, result string) {
 	var args CmdLineArgs
 	if success {
 		if strings.HasPrefix(result, successLine) {
-			args.Args = strings.ReplaceAll(result[len(successLine):], "\n", " ")
-		} else {
-			args.Args = result
+			result = strings.ReplaceAll(result[len(successLine):], "\n", " ")
 		}
+		args.Args = result
 		args.Success = true
 	} else {
-		args.Success = false
 		args.Error = result
 	}
-	if err := templ.ExecuteTemplate(w, "cmd_line.html", args); err != nil {
-		fmt.Fprintf(w, "Executing cmd_line template: %v\n", err)
+	if err := tmpl.ExecuteTemplate(w, "cmd_line.html", args); err != nil {
+		fmt.Fprintf(w, "Failed executing cmd_line template: %v\n", err)
 	}
 }
