9537148496a964f4490b8dba21f8eff9a710fbff
Dionysis Ntreou <yakizarns@hotmail.com>
2023-05-27 07:09:50 +0300

    Update helpers.go

--- api/helpers.go
+++ api/helpers.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"fmt"
-	"github.com/ledgerwatch/diagnostics"
 	"github.com/pkg/errors"
 	"log"
 	"net/http"
@@ -14,15 +12,15 @@ import (
 func retrievePinFromURL(r *http.Request) (uint64, error) {
 	parsedURL, err := url.Parse(r.URL.Path)
 	if err != nil {
-		fmt.Println("Error parsing URL:", err)
-		return 0, err
+		log.Println("Error parsing URL:", err)
+		return 0, errors.Wrap(err, "Error parsing URL")
 	}
 
 	lastPathItem := path.Base(parsedURL.Path)
 	pin, err := strconv.ParseUint(lastPathItem, 10, 64)
 	if err != nil {
-		log.Printf("Errir parsing session pin %s: %v\n", lastPathItem, err)
-		return 0, err
+		log.Printf("Error parsing session pin %s: %v\n", lastPathItem, err)
+		return 0, errors.Wrapf(err, "Error parsing session pin %s", lastPathItem)
 	}
 
 	return pin, nil
@@ -32,7 +30,7 @@ func retrieveSizeStrFrom(r *http.Request) (uint64, error) {
 	sizeStr := r.Form.Get("size")
 	size, err := strconv.ParseUint(sizeStr, 10, 64)
 	if err != nil {
-		return 0, diagnostics.AsBadRequestErr(errors.Errorf("Parsing size %s: %v", sizeStr, err))
+		return 0, errors.Wrapf(err, "Parsing size %s", sizeStr)
 	}
 
 	var offset uint64
