3095a716d75af386729897eee78e1bf6ed45f697
Artem Bogomaz <artembogomaz@gmail.com>
2023-06-01 22:33:35 +0700

    NOTICKET: improving reorgs module (#17)
    * NOTICKET: improving reorgs module
    
    * NOTICKET: goimports run

--- internal/erigon_node/reorg.go
+++ internal/erigon_node/reorg.go
@@ -1,68 +1,113 @@
 package erigon_node
 
 import (
-	"bytes"
 	"context"
 	"encoding/binary"
 	"fmt"
-	"github.com/ledgerwatch/diagnostics/internal"
 	"html/template"
+	"io"
 	"net/http"
 	"time"
+
+	"github.com/ledgerwatch/diagnostics/internal"
 )
 
 // Demonstration of the working with the Erigon database remotely on the example of getting information
 // about past reorganisation of the chain
-const headersDb = "chaindata"
-const headersTable = "Header"
+const (
+	headersDb    = "chaindata"
+	headersTable = "Header"
+	maxCount     = 1000
+)
 
-func (c *NodeClient) FindReorgs(ctx context.Context, w http.ResponseWriter, template *template.Template, requestChannel chan *internal.NodeRequest) {
+// FindReorgs - Go through "Header" table and look for entries with the same block number but different hashes
+func (c *NodeClient) FindReorgs(ctx context.Context,
+	writer http.ResponseWriter,
+	template *template.Template,
+	requestChannel chan *internal.NodeRequest) {
 	start := time.Now()
+	var err error
 
 	rc := NewRemoteCursor(c, requestChannel)
-
-	if err := rc.Init(headersDb, headersTable, nil); err != nil {
-		fmt.Fprintf(w, "Create remote cursor: %v", err)
+	if err = rc.Init(headersDb, headersTable, nil); err != nil {
+		fmt.Fprintf(writer, "Create remote cursor: %v", err)
 		return
 	}
 
-	// Go through "Header" table and look for entries with the same block number but different hashes
-	var prevK []byte
-	reorgCount := 0
+	total, wrongBlocks, errors := c.findReorgsInternally(ctx, template, rc)
+	for _, err := range errors {
+		if err != nil {
+			fmt.Fprintf(writer, "%v\n", err)
+		}
+	}
+
+	fmt.Fprintf(writer, "Reorg iterator: %d, total scanned %s\n", len(total), time.Since(start))
+	fmt.Fprintf(writer, "Reorg iterator: %d, wrong blocks\n", len(wrongBlocks))
+}
+
+func (c *NodeClient) executeFlush(writer io.Writer,
+	template *template.Template,
+	name string, data any) error {
+	if err := template.ExecuteTemplate(writer, name, data); err != nil {
+		return err
+	}
+	if f, ok := writer.(http.Flusher); ok {
+		f.Flush()
+	}
+	return nil
+}
+
+// findReorgsInternally - searching for reorgs,
+// return back total blocks set and wrong blocks
+// if there are errors in the middle of processing will return back
+// slice of errors
+func (c *NodeClient) findReorgsInternally(ctx context.Context,
+	template *template.Template,
+	rc *RemoteCursor,
+) (map[uint64][]byte, []uint64, []error) {
+	var errors []error
+	set := make(map[uint64][]byte)
+	var wrongBlocks []uint64
+
 	var k []byte
-	var e error
-	var count int
-	for k, _, e = rc.Next(); e == nil && k != nil; k, _, e = rc.Next() {
+
+	var iterator int
+	var err error
+	for k, _, err = rc.Next(); err == nil && k != nil; k, _, err = rc.Next() {
 		select {
 		case <-ctx.Done():
-			fmt.Fprintf(w, "Interrupted\n")
-			return
+			return nil, nil, []error{fmt.Errorf("Interrupted\n")}
 		default:
 		}
-		if len(k) >= 8 && len(prevK) >= 8 && bytes.Equal(k[:8], prevK[:8]) {
-			bn := binary.BigEndian.Uint64(k[:8])
-			if err := template.ExecuteTemplate(w, "reorg_block.html", bn); err != nil {
-				fmt.Fprintf(w, "Executing reorg_block template: %v\n", err)
-			}
-			if f, ok := w.(http.Flusher); ok {
-				f.Flush()
-			}
-			reorgCount++
+
+		if len(k) == 0 {
+			continue
 		}
-		prevK = k
-		count++
-		if count%1000 == 0 {
-			if err := template.ExecuteTemplate(w, "reorg_spacer.html", nil); err != nil {
-				fmt.Fprintf(w, "Executing reorg_spacer template: %v\n", err)
+
+		bn := binary.BigEndian.Uint64(k[:8])
+		_, found := set[bn]
+		if found {
+			if template != nil {
+				if err := c.executeFlush(nil, template, "reorg_block.html", bn); err != nil {
+					errors = append(errors, fmt.Errorf("Executing reorg_block template: %v\n", err))
+				}
 			}
-			if f, ok := w.(http.Flusher); ok {
-				f.Flush()
+			wrongBlocks = append(wrongBlocks, bn)
+		}
+		set[bn] = k
+
+		iterator++
+		if iterator%maxCount == 0 {
+			if template != nil {
+				if err := c.executeFlush(nil, template, "reorg_block.html", bn); err != nil {
+					errors = append(errors, fmt.Errorf("Executing reorg_spacer template: %v\n", err))
+				}
 			}
 		}
 	}
-	if e != nil {
-		fmt.Fprintf(w, "Process remote cursor line: %v\n", e)
-		return
+	if err != nil {
+		errors = append(errors, err)
 	}
-	fmt.Fprintf(w, "Reorg count: %d, produced in %s\n", reorgCount, time.Since(start))
+
+	return set, wrongBlocks, errors
 }
