121c99a98534e4b8ac30225174d69ba0a9fdfb5c
dvovk <vovk.dimon@gmail.com>
2023-10-13 17:02:48 +0300

    changed peer data structure

--- internal/erigon_node/types.go
+++ internal/erigon_node/types.go
@@ -9,20 +9,27 @@ type Versions struct {
 	GitCommit      string `json:"gitCommit"`
 }
 
+type PeerNetworkInfo struct {
+	LocalAddress  string `json:"localAddress"`  // Local endpoint of the TCP data connection
+	RemoteAddress string `json:"remoteAddress"` // Remote endpoint of the TCP data connection
+	Inbound       bool   `json:"inbound"`
+	Trusted       bool   `json:"trusted"`
+	Static        bool   `json:"static"`
+}
+
 type PeerInfo struct {
-	ENR     string   `json:"enr,omitempty"` // Ethereum Node Record
-	Enode   string   `json:"enode"`         // Node URL
-	ID      string   `json:"id"`            // Unique node identifier
-	Name    string   `json:"name"`          // Name of the node, including client type, version, OS, custom data
-	Caps    []string `json:"caps"`          // Protocols advertised by this peer
-	Network struct {
-		LocalAddress  string `json:"localAddress"`  // Local endpoint of the TCP data connection
-		RemoteAddress string `json:"remoteAddress"` // Remote endpoint of the TCP data connection
-		Inbound       bool   `json:"inbound"`
-		Trusted       bool   `json:"trusted"`
-		Static        bool   `json:"static"`
-	} `json:"network"`
-	Protocols map[string]interface{} `json:"protocols"` // Sub-protocol specific metadata fields
+	ENR           string                 `json:"enr,omitempty"` // Ethereum Node Record
+	Enode         string                 `json:"enode"`         // Node URL
+	ID            string                 `json:"id"`            // Unique node identifier
+	Name          string                 `json:"name"`          // Name of the node, including client type, version, OS, custom data
+	ErrorCount    int                    `json:"errorCount"`    // Number of errors
+	LastSeenError string                 `json:"lastSeenError"` // Last seen error
+	Type          string                 `json:"type"`          // Type of connection
+	Caps          []string               `json:"caps"`          // Protocols advertised by this peer
+	Network       PeerNetworkInfo        `json:"network"`
+	Protocols     map[string]interface{} `json:"protocols"` // Sub-protocol specific metadata fields
+	BytesIn       int                    `json:"bytesIn"`   // Number of bytes received from the peer
+	BytesOut      int                    `json:"bytesOut"`  // Number of bytes sent to the peer
 }
 
 type PeersInfo []PeerInfo
