b86fe7b972487e07b50a33017e79b27e928bf5ab
dvovk <vovk.dimon@gmail.com>
2023-11-06 16:10:30 +0300

    updated diagnostics to pass throug map of snapshot sync data

--- internal/erigon_node/snapshots.go
+++ internal/erigon_node/snapshots.go
@@ -5,22 +5,7 @@ import (
 	"encoding/json"
 )
 
-type DownloadStatistics struct {
-	Progress         string `json:"progress"`
-	Downloaded       string `json:"downloaded"`
-	Total            string `json:"total"`
-	TimeLeft         string `json:"timeLeft"`
-	TotalTime        string `json:"totalTime"`
-	DownloadRate     string `json:"downloadRate"`
-	UploadRate       string `json:"uploadRate"`
-	Peers            int32  `json:"peers"`
-	Files            int32  `json:"files"`
-	Connections      uint64 `json:"connections"`
-	Alloc            string `json:"alloc"`
-	Sys              string `json:"sys"`
-	DownloadFinished bool   `json:"downloadFinished"`
-	StagePrefix      string `json:"stagePrefix"`
-}
+type DownloadStatistics map[string]interface{}
 
 // FindReorgs - Go through "Header" table and look for entries with the same block number but different hashes
 func (c *NodeClient) ShanphotSync(ctx context.Context) (DownloadStatistics, error) {
