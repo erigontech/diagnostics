1e80113efece0ba768e9f1c4dff2197cb716da53
alex.sharov <AskAlexSharov@gmail.com>
2024-10-16 12:47:41 +0700

    save

--- internal/sessions/helpers.go
+++ internal/sessions/helpers.go
@@ -9,8 +9,8 @@ func generatePIN() (uint64, error) {
 	//if insecure { TODO fix this
 	//	return uint64(weakrand.Int63n(100_000_000)), nil
 	//}
-	max := big.NewInt(100_000_000) // For an 8-digit PIN
-	randNum, err := rand.Int(rand.Reader, max)
+	_max := big.NewInt(100_000_000) // For an 8-digit PIN
+	randNum, err := rand.Int(rand.Reader, _max)
 	if err != nil {
 		return 0, err
 	}
